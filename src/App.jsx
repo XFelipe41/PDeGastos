1e82852160f755d14689ea6cc34ee2332d2c0fae
XFelipe41 <jfcaicedot@ut.edu.co>
2023-09-06 11:57:44 -0500

    Nuevo Proyecto Planificador de Gastos

---
+++ src/App.jsx
@@ -0,0 +1,146 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+import Filtros from "./components/Filtros";
+import ListadoGasto from "./components/ListadoGasto";
+import { generarId } from "./helpers";
+import NuevoPresupuesto from "./components/NuevoPresupuesto";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
+
+function App() {
+  const [gastos, setGastos] = useState(
+    localStorage.getItem("gastos")
+      ? JSON.parse(localStorage.getItem("gastos"))
+      : []
+  );
+
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem("presupuesto")) ?? 0
+  );
+  const [isVlidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+  const [gastoEditar, setGastoEditar] = useState({});
+
+  const [filtro, setFiltro] = useState("");
+  const [gastosFiltrados, setgastosFiltrados] = useState([]);
+
+  useEffect(() => {
+    if (Object.keys(gastoEditar).length > 0) {
+      setModal(true);
+
+      setTimeout(() => {
+        setAnimarModal(true);
+      }, 500);
+    }
+  }, [gastoEditar]);
+
+  useEffect(() => {
+    localStorage.setItem("presupuesto", presupuesto ?? 0);
+  }, [presupuesto]);
+
+  useEffect(() => {
+    localStorage.setItem("gastos", JSON.stringify(gastos) ?? []);
+  }, [gastos]);
+
+  useEffect(() => {
+    if (filtro) {
+      //Actualizar gastos por categoria
+      const gastosFiltrados = gastos.filter(
+        (gasto) => gasto.categoria === filtro
+      );
+      setgastosFiltrados(gastosFiltrados);
+    }
+  }, [filtro]);
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem("presupuesto")) ?? 0;
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true);
+    }
+  }, []);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+    setGastoEditar({});
+
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+  };
+
+  const guardarGasto = (gasto) => {
+    if (gasto.id) {
+      //Actualizar
+      const gastosActualizados = gastos.map((gastoState) =>
+        gastoState.id === gasto.id ? gasto : gastoState
+      );
+      setGastos(gastosActualizados);
+      setGastoEditar({});
+    } else {
+      // NUevo Gasto
+      gasto.id = generarId();
+      gasto.fecha = Date.now();
+      setGastos([...gastos, gasto]);
+    }
+
+    setAnimarModal(false);
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+  };
+
+  const eliminarGasto = (id) => {
+    const gastosActualizados = gastos.filter((gasto) => gasto.id !== id);
+    setGastos(gastosActualizados);
+  };
+
+  return (
+    <div className={modal ? "fijar" : ""}>
+      <Header
+        gastos={gastos}
+        setGastos={setGastos}
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isVlidPresupuesto={isVlidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isVlidPresupuesto && (
+        <>
+          <main>
+            <Filtros filtro={filtro} setFiltro={setFiltro} />
+            <ListadoGasto
+              gastos={gastos}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt="icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+      )}
+
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+          gastoEditar={gastoEditar}
+          setGastoEditar={setGastoEditar}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
